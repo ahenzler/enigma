a9af6a0421698317d24b8c6714a4a1b3b12d1807
ahenzler <aliciahenzler@outlook.com>
2021-04-25 17:39:12 -0600

    set up file io

---
+++ lib/decrypt.rb
@@ -0,0 +1,17 @@
+require './lib/enigma'
+
+enigma = Enigma.new
+
+handle = File.open(ARGV[0], "r")
+
+message = handle.read
+
+handle.close
+
+encrypted_messages = enigma.encrypt(message, "01234","081493")
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(encrypted_messages)
+
+writer.close
